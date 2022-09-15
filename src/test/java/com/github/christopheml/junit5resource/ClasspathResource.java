03892fb1d465133dc54d90872f668e7f253ac5a3
Christophe Michel <christopheml@users.noreply.github.com>
2022-09-16 00:59:49 +0200

    Fixes missing runtime retention on the annotation.

--- src/test/java/com/github/christopheml/junit5resource/ClasspathResource.java
+++ src/test/java/com/github/christopheml/junit5resource/ClasspathResource.java
@@ -1,8 +1,13 @@
 package com.github.christopheml.junit5resource;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ClasspathResource {
 
     String value();
+
     String charset() default "UTF-8";
 
 }
